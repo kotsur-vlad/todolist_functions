24ac1011fec5e0b8c1cd17b06770bed8a188b94f
kotsur-vlad <kotsur.vlad@gmail.com>
2020-07-28 01:52:31 +0300

    Created adding new task, refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React, {useState} from "react"
 
 import "./App.css"
 import {TodoList} from "./TodoList"
+import {v1} from "uuid"
 
 export type FilterValuesType = "all" | "active" | "completed"
 
 export type TaskType = {
-	id: number
+	id: string
 	title: string
 	isDone: boolean
 }
@@ -15,17 +16,28 @@ function App () {
 
 	//Get initial tasks
 	const initTasks = [
-		{id: 1, title: "HTML&CSS", isDone: true},
-		{id: 2, title: "JS", isDone: true},
-		{id: 3, title: "React", isDone: false},
-		{id: 4, title: "Redux", isDone: false},
-		{id: 5, title: "GraphQL", isDone: false},
+		{id: v1(), title: "HTML&CSS", isDone: true},
+		{id: v1(), title: "JS", isDone: true},
+		{id: v1(), title: "React", isDone: false},
+		{id: v1(), title: "Redux", isDone: false},
+		{id: v1(), title: "GraphQL", isDone: false},
 	]
 
 	const [tasks, setTasks] = useState<Array<TaskType>>(initTasks)
 
+	//Add task
+	const addTask = (taskTitle: string) => {
+		const task = {
+			id: v1(),
+			title: taskTitle,
+			isDone: false
+		}
+		const newTasks = [task, ...tasks]
+		setTasks(newTasks);
+	}
+
 	//Remove task
-	const removeTask = (taskId: number) => {
+	const removeTask = (taskId: string) => {
 		const newTasks = tasks.filter(t => t.id !== taskId)
 		setTasks(newTasks);
 	}
@@ -49,7 +61,8 @@ function App () {
             <TodoList title="What to learn"
 					  tasks={filteredTasks}
 					  removeTask={removeTask}
-					  changeFilter={changeFilter}/>
+					  changeFilter={changeFilter}
+					  addTask={addTask}/>
 		</div>
 	)
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,40 +1,65 @@
-import React from "react"
+import React, {ChangeEvent, KeyboardEvent, useState} from "react"
 
 import {FilterValuesType, TaskType} from "./App"
 
 type PropsType = {
 	title: string
 	tasks: Array<TaskType>
-	removeTask: (taskId: number) => void
+	removeTask: (taskId: string) => void
 	changeFilter: (filterValue: FilterValuesType) => void
+	addTask: (taskTitle: string) => void
 }
 
 export function TodoList (props: PropsType) {
 
-	// const onRemoveTaskButtonClick = () => {
-	// 	props.removeTask(t.id)
-	// }
+	const [newTaskTitle, setNewTaskTitle] = useState("")
+
+	const addTask = () => {
+		props.addTask(newTaskTitle)
+		setNewTaskTitle("")
+	}
+
+	const onNewTaskTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setNewTaskTitle(e.currentTarget.value)
+	}
+
+	const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.charCode === 13) {
+			addTask()
+		}
+	}
+
+	const onAllClickHandler = () => props.changeFilter("all")
+	const onActiveClickHandler = () => props.changeFilter("active")
+	const onCompletedClickHandler = () => props.changeFilter("completed")
 
 	return (
 		<div>
 			<h3>{props.title}</h3>
 			<div>
-				<input/>
-				<button>+</button>
+				<input value={newTaskTitle}
+					   onChange={onNewTaskTitleChangeHandler}
+					   onKeyPress={onKeyPressHandler}/>
+				<button onClick={addTask}>+</button>
 			</div>
 			<ul>
 				{
-					props.tasks.map(t => <li key={t.id}>
-						<input type="checkbox" checked={t.isDone}/>
-						<span>{t.title}</span>
-						<button onClick={() => {props.removeTask(t.id)}}>x</button>
-					</li>)
+					props.tasks.map(t => {
+						const onRemoveHandler = () => props.removeTask(t.id)
+
+						return <li key={t.id}>
+							<input type="checkbox"
+								   checked={t.isDone}/>
+							<span>{t.title}</span>
+							<button onClick={onRemoveHandler}>x</button>
+						</li>
+					})
 				}
 			</ul>
 			<div>
-				<button onClick={() => {props.changeFilter("all")}}>All</button>
-				<button onClick={() => {props.changeFilter("active")}}>Active</button>
-				<button onClick={() => {props.changeFilter("completed")}}>Completed</button>
+				<button onClick={onAllClickHandler}>All</button>
+				<button onClick={onActiveClickHandler}>Active</button>
+				<button onClick={onCompletedClickHandler}>Completed</button>
 			</div>
 		</div>
 	)
